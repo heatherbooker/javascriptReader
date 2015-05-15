d4b892c06ed524bad5fbc2894fa860bee23fab67
Heather Booker <heather.booker@hotmail.com>
2015-05-15 15:02:29 -0700

    defined buttons and input and output areas

---
+++ script.js
@@ -0,0 +1,5 @@
+function runInput() {
+    var inputScript = document.getElementById("myText").value;
+    console.log(inputScript)
+    document.getElementById("output").innerHTML = inputScript;
+}
