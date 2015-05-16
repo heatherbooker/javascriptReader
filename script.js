848ddcde3a4562f6f0aa0ddab5ffe7b9818875f7
Heather Booker <heather.booker@hotmail.com>
2015-05-15 17:22:45 -0700

    semi functional clear buttons

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function runInput() {
-    var inputScript = document.getElementById("myText").value;
+    var inputScript = document.getElementById("input").value;
     console.log(inputScript)
     document.getElementById("output").innerHTML = inputScript;
+}
+
+function clearInput() {
+    inputScript = "";
+}
+
+function clearOutput() {
+    document.getElementById("output").innerHTML = "";
 }
